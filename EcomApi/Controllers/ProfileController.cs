5f837b69257b3f49a4a4249955d564c75992065b
Vayasandesu <vayafamiglia@gmail.com>
2020-09-11 11:59:00 +0700

    Profile

---
+++ EcomApi/Controllers/ProfileController.cs
@@ -0,0 +1,50 @@
+﻿using EcomApi.Database;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace EcomApi.Controllers {
+
+	[ApiController]
+	[Route("api/[controller]")]
+	public class ProfileController : ControllerBase {
+		
+		[HttpGet("{user}")]
+		public string GetProfile(string user) {
+			var db = Firebase.Database;
+			var snap = db.Collection("Profiles").Document(user).GetSnapshotAsync().Result;
+			if(snap.Exists) {
+				snap.TryGetValue<string>("Slug", out var slug);
+				return "profile : " + slug;
+			} 
+			return "profile : ";
+		}
+
+		// 
+		[HttpPatch("edit")]
+		public string Edit([FromBody] Dictionary<string, string> value) {
+			value.TryGetValue("Username", out var user);
+			value.TryGetValue("Slug", out var slug);
+
+			var db = Firebase.Database;
+			var collection = db.Collection("Profiles");
+			var snap = collection.Document(user);
+
+			var data = new Dictionary<string, object>();
+			data.Add("Slug", slug);
+
+			if(snap.GetSnapshotAsync().Result.Exists) {
+				//Edit
+				snap.UpdateAsync(data);
+				return "edit completed";
+
+			} else {
+				// Create
+				var doc = collection.Document(user).SetAsync(data);
+				return "edit completed";
+			}
+
+		}
+
+	}
+
+}
