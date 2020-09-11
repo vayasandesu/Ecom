90b5070b106e63c5a61e532f4f8a3fbbcd2202e1
Vayasandesu <vayafamiglia@gmail.com>
2020-09-11 12:41:50 +0700

    responser

---
+++ EcomApi/Models/JsonResponser.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace EcomApi.Models {
+	public class JsonResponser {
+		public static string Response(bool isSuccess, string Message) {
+			JObject j = new JObject();
+			j.Add("result", isSuccess);
+			j.Add("message", Message);
+			return JsonConvert.SerializeObject(j);
+		}
+	}
+}
