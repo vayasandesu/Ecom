1609b7265045ba2f89895f513bba3272813b2db5
Vayasandesu <vayafamiglia@gmail.com>
2020-09-11 11:15:01 +0700

    Add db

---
+++ EcomApi/Database/Firebase.cs
@@ -0,0 +1,27 @@
+﻿using Google.Cloud.Firestore;
+using System;
+
+namespace EcomApi.Database {
+
+	public class Firebase {
+
+		public const string PROJECT_ID = "lacia-project-deal";
+		public const string GOOGLE_APPLICATION_CREDENTIALS = "./Resources/firebase-key.json";
+
+		/// <summary>
+		/// Create new instance of database
+		/// </summary>
+		public static FirestoreDb Database {
+			get {
+				FirestoreDb db = FirestoreDb.Create(EcomApi.Database.Firebase.PROJECT_ID);
+				return db;
+			}
+		}
+
+		public static void SetupEnvironment() {
+			Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", EcomApi.Database.Firebase.GOOGLE_APPLICATION_CREDENTIALS);
+		}
+
+	}
+
+}
--- EcomApi/Startup.cs
+++ EcomApi/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using EcomApi.Database;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace EcomApi {
 	public class Startup {
@@ -21,6 +15,7 @@ namespace EcomApi {
 
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services) {
+			Firebase.SetupEnvironment();
 			services.AddControllers();
 		}
 
